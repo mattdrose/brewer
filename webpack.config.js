0d2b936103e88b94828fa1c54713e39c883d36e0
Matt Rose <rose.matt@gmail.com>
2018-03-20 11:05:42 -0400

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'production',
+  entry: ['./main.js', './css.js'],
+  module: {
+    rules: [
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inlineSource: '.(js|css)$',
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+  ],
+};
