5cc9196eed30396f58c54a3975c1089dddb638a4
Matt Rose <rose.matt@gmail.com>
2019-03-08 17:29:54 -0500

    Setup handlebar templating and image object generation

---
+++ scripts/resources.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary');
+const _ = require('lodash');
+
+cloudinary.config({
+  cloud_name: 'mdr',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+function resources() {
+  return new Promise((resolve, reject) => {
+    cloudinary.v2.api.resources({ 
+      type: 'upload',
+      prefix: 'brewer',
+      context: true,
+    }, (error, result) => {
+      if(error || !result.resources) {
+        reject(error);
+      } else {
+        resolve(result.resources);
+      }
+    });
+  });
+}
+
+async function images() {
+  return resources().then((data) => data.map((image) => ({
+    title: _.get(image, 'context.custom.title') || '',
+    contribution: _.get(image, 'context.custom.contribution') || '',
+    alt: _.get(image, 'context.custom.alt') || '',
+    sizes: [400, 600, 800, 1000, 1200, 1400].reduce((acc, width) => {
+      acc[width] = cloudinary.url(image.public_id, {
+        secure: true,
+        width,
+      });
+      return acc;
+    }, {}),
+  })));
+}
+
+module.exports = {images};
--- webpack.config.js
+++ webpack.config.js
@@ -7,40 +7,45 @@ const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const resources = require('./scripts/resources');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  devServer: {
-    open: true,
-  },
-  entry: './main.js',
-  mode: process.env.NODE_ENV || 'production',
-  module: {
-    rules: [
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+module.exports = resources.images().then((images) => {
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.js',
+    mode: process.env.NODE_ENV || 'production',
+    module: {
+      rules: [
+        { test: /\.hbs$/, loader: 'handlebars-loader' },
+        { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+      ],
+    },
+    optimization: {
+      minimize: true,
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Homepage',
+        filename: 'index.html',
+        template: 'index.hbs',
+        templateParameters: {
+          images,
+          imagesString: JSON.stringify(images),
+        },
+        cache: false,
+        inlineSource: '.(js|css)$',
+        alwaysWriteToDisk: true,
+      }),
+      new HtmlWebpackHarddiskPlugin(),
+      new HtmlWebpackInlineSourcePlugin(),
+      new OptimizeCssAssetsPlugin(),
+      new CopyPlugin([
+        { from: 'fonts', to: 'fonts' },
+      ]),
+      new webpack.DefinePlugin({
+        LAMBDA_ENDPOINT: JSON.stringify(process.env.LAMBDA_ENDPOINT),
+      }),
     ],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      cache: false,
-      inlineSource: '.(js|css)$',
-      alwaysWriteToDisk: true,
-    }),
-    new HtmlWebpackHarddiskPlugin(),
-    new HtmlWebpackInlineSourcePlugin(),
-    new OptimizeCssAssetsPlugin(),
-    new CopyPlugin([
-      { from: 'fonts', to: 'fonts' },
-    ]),
-    new webpack.DefinePlugin({
-      LAMBDA_ENDPOINT: JSON.stringify(process.env.LAMBDA_ENDPOINT),
-    }),
-  ],
-};
+  };
+})
