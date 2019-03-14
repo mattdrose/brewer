899d0dde1d89338803ac4d00722bab51bb299c03
Matt Rose <rose.matt@gmail.com>
2019-03-14 14:13:02 -0400

    Add basic gallery functionality

---
+++ helpers/tokenize.js
@@ -0,0 +1,5 @@
+module.exports = function(value) {
+  value = '' + value;
+  const valueArr = value.toLowerCase().match(/\S+/g) || [];
+  return valueArr.join('-');
+};
--- src/main.js
+++ src/main.js
@@ -1,2 +1,67 @@
 import './main.scss';
 import 'lazySizes';
+
+const $ = document.querySelector.bind(document);
+const nodeListToArray = function(nodeList) { return [].slice.call(nodeList); }
+
+class Gallery {
+  constructor(el) {
+    this.el = el;
+    this.itemEls = el.querySelectorAll('[data-gallery-item]');
+    this.items = nodeListToArray(this.itemEls).map((itemEl) => {
+      return new Gallery.Item(itemEl)
+    });
+  }
+
+  get currentIndex() {
+    return this.items.findIndex((item) => item.isActive());
+  }
+
+  get current() {
+    return this.items[this.currentIndex];
+  }
+
+  set current(index) {
+    this.current.deactivate();
+    this.items[index].activate();
+  }
+
+  next() {
+    let nextIndex = this.currentIndex + 1;
+    if (nextIndex >= this.items.length) { nextIndex = 0; }
+    this.current = nextIndex;
+  }
+
+  init() {
+    this.el.addEventListener('click', this.next.bind(this));
+  }
+}
+
+Gallery.Item = class {
+  constructor(el) {
+    this.el = el;
+    this.classes = {
+      active: 'is-active',
+    };
+  }
+
+  get token() {
+    return this.el.dataset.galleryToken;
+  }
+
+  isActive() {
+    return this.el.classList.contains(this.classes.active);
+  }
+
+  activate() {
+    this.el.classList.add(this.classes.active);
+  }
+
+  deactivate() {
+    this.el.classList.remove(this.classes.active);
+  }
+}
+
+const gallery = new Gallery($('[data-gallery]'))
+gallery.init();
+window.debug = gallery;
