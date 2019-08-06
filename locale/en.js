1047a370f95009d76694bfa0bf053fe6026121b0
Michael Brewer <michael.brewer+shopify@shopify.com>
2019-08-06 16:15:38 -0400

    Update en.js

--- locale/en.js
+++ locale/en.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Michael Brewer handles the creation of elegant and useful
 experiences across all mediums at Shopify in Toronto, Canada.`,
     subhead: 'Simple & Useful',
-    dribbble: 'Dribble',
+    dribbble: 'Dribbble',
     behance: 'Behance',
     details: {
       name: 'Michael Brewer',
