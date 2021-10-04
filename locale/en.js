acde85c276950340cc6330f07582318a4c262069
michaelbrewer100 <michael.brewer+shopify@shopify.com>
2021-10-04 17:18:34 -0400

    Workplace update
    updated workplace from Shopify to Wealthsimple

--- locale/en.js
+++ locale/en.js
@@ -1,14 +1,14 @@
 module.exports = {
   en: {
     title: 'Michael Brewer | Designer | Toronto, Canada',
-    description: `Michael Brewer is a Senior Designer with a focus on 
-    creating impactful experiences through elegant creative and artistic direction, currently working at Shopify.`,
+    description: `Michael Brewer is a Staff Brand Designer with a focus on 
+    creating impactful experiences through elegant creative and artistic direction, currently working at Wealthsimple.`,
     subhead: 'Elegant & Useful',
     dribbble: 'Dribbble',
     behance: 'Behance',
     details: {
       name: 'Michael Brewer',
-      company: 'Shopify',
+      company: 'Wealthsimple',
       location: 'Toronto, Canada',
       email: 'michael.edmund@gmail.com',
       phone: '+1 416 333 7884'
@@ -21,8 +21,8 @@ module.exports = {
     about: {
       title: 'Michael Brewer | About',
       heading: 'About',
-      content: `Michael Brewer is a Senior Designer with a focus on 
-      creating impactful experiences through elegant creative and artistic direction, currently working at Shopify.`
+      content: `Michael Brewer is a Staff Brand Designer with a focus on 
+      creating impactful experiences through elegant creative and artistic direction, currently working at Wealthsimple.`
     },
     contact: {
       title: 'Michael Brewer | Contact',
